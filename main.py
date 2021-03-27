1184b4d7ee0cca82811619d5b73dee13b9409382
Dev Raj <rehansingh.4522@gmail.com>
2021-03-27 11:45:01 +0530

    add more routes

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 
-
 @app.route('/')
 def index():
     return f"<h1>Magic Book Shelf</h1>"
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
-    return f"<h1>Magic Book Shelf</h1>"
+    return f"<h1>Magic Book Shelf - Login</h1>"
 
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    return f"<h1>Magic Book Shel - SignUp</h1>"
+
+@app.route('/list_book', methods=["GET", "POST"])
+def list_book():
+    return f"<h1>List Book for donation</h1>"
+
+
+@app.route("books/<int:id>/", methods=["GET", "POST"])
+def book(id):
+    return f"<h1>Good to Great - By Jim Collins</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
