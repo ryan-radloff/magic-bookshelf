163d6a53e4e2b580bd4e46deb83a4436afa0fd2c
Dev Raj <rehansingh.4522@gmail.com>
2021-03-27 11:06:27 +0530

    Add template structure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -8,6 +9,11 @@ app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 @app.route('/')
 def index():
     return f"<h1>Magic Book Shelf</h1>"
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    return f"<h1>Magic Book Shelf</h1>"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
