0518cb87e2a98c55dee1a9e47d7031b982d4a37b
caleb park <calebparky@gmail.com>
2021-03-27 00:45:14 -0500

    template directory

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
 
-
 @app.route('/')
 def index():
-    return f"<h1>Magic Book Shelf</h1>"
+    return render_template('index.html')
+
+
+@app.route("/<string:name>/profile")
+def show_profile(name):
+    return render_template('profile.html', name=name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
