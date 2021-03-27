5bdc266c3775b8cf5f9e3ba933be3c35875f7826
Dev Raj Singh <55313450+devraj4522@users.noreply.github.com>
2021-03-27 19:57:39 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,7 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    submit = SubmitField("Let Me In!")
+    submit = SubmitField("Submit")
+
+
+class RegisterForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class BookForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    author = StringField("Author", validators=[DataRequired()])
+    isbn = StringField("ISBN", validators=[DataRequired()])
+    submit = SubmitField("Submit")
