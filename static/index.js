73ee8dad41575e6b4db9f7960185377eb5d8b2f0
caleb park <calebparky@gmail.com>
2021-03-28 00:46:20 -0500

    cool background gradient effects

---
+++ static/index.js
@@ -0,0 +1,26 @@
+let w = $(window).width();
+let h = $(window).height();
+
+let hue = 0;
+let mouseXpercentage;
+let mouseYpercentage;
+
+$(document).mousemove(function(event) {
+    mouseXpercentage = Math.round(event.pageX / w * 100);
+    mouseYpercentage = Math.round(event.pageY / h * 100);
+    
+    // var innerColor = "hsl(0, 88%, 51%)";
+    // var outerColor = "hsl(0, 77%, 15%)";
+    updateBackground();
+});
+
+setInterval(() => {
+    hue = (++hue) % 360;
+    //console.log(hue, mouseXpercentage, mouseYpercentage);
+    updateBackground();
+}, 50);
+
+function updateBackground() {
+    $('.background').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' 
+    + mouseYpercentage + '%, hsl(' + hue + ', 88%, 51%), hsl(' + ((hue + 180) % 360) + ', 100%, 25%)');
+}
