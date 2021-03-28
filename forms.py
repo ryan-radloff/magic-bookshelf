10539fe29e222c4bfdc19b1f55b1997f5e94fc16
Radster859 <31903875+Radster859@users.noreply.github.com>
2021-03-27 20:18:56 -0500

    Working Commit 1

--- forms.py
+++ forms.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Email
+from wtforms import StringField, SubmitField, PasswordField, IntegerField
+from wtforms.validators import DataRequired, Email, Length, Regexp, ValidationError
+import requests
 
+def book_exists(form, field):
+    if(requests.get("https://www.googleapis.com/books/v1/volumes?q=isbn:" + field.data).json()["totalItems"] == 0):
+        raise ValidationError("ISBN not found. Try alternate format?")
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -17,7 +21,6 @@ class RegisterForm(FlaskForm):
 
 
 class BookForm(FlaskForm):
-    title = StringField("Title", validators=[DataRequired()])
-    author = StringField("Author", validators=[DataRequired()])
-    isbn = StringField("ISBN", validators=[DataRequired()])
+    isbn = StringField("ISBN", validators=[DataRequired(), Regexp("\d{13}|\d{10}"), book_exists])
     submit = SubmitField("Submit")
+
--- main.py
+++ main.py
@@ -2,18 +2,35 @@ from flask import Flask, render_template, redirect, url_for, request
 from forms import LoginForm, RegisterForm, BookForm
 from flask_bootstrap import Bootstrap
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
 
+# TODO: figure out whats wrong with python-dotenv because it returns every string with quotes
+load_dotenv()
 app = Flask(__name__)
+Base = declarative_base()
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
 Bootstrap(app)
 
+Session = sessionmaker()
 # Sqlalchamy database
 # TODO: Change the SQL database when done
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+s = "mysql+mysqlconnector://{username}:{password}@{server}/bookshelf_db".format(username = "root", password = os.getenv('PASSWORD'), server = "104.197.168.142:3306")
+engine = create_engine(s)
+engine.connect()
+
+
+Session.configure(bind=engine)
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://{username}:{password}@{server}/bookshelf_db".format(username = "root", password = "p@xs8Ddz4.YVT-QweUZE", server = "104.197.168.142:3306")
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#db = SQLAlchemy(app)
 
 # Login 
 login_manager = LoginManager()
@@ -21,28 +38,29 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    session = Session()
+    return session.query(User).filter(User.user_id == user_id).first()
 
 
 ##CREATE TABLE IN DB
-class User(UserMixin, db.Model):
-    user_id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(1000))
-
+class User(UserMixin, Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    email = Column(String(250))
+    username = Column(String(25), unique=True)
+    password = Column(String(1000))
+    is_authenticated = True
     def get_id(self):
            return (self.user_id)
 
-class Book(db.Model):
-    book_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    author = db.Column(db.String(100))
-    isbn = db.Column(db.String(100))
-    owner = db.Column(db.String(100))
-
+class Book(Base):
+    __tablename__ = 'books'
+    book_id = Column(Integer, primary_key=True)
+    isbn = Column(Integer)
+    owner = Column(Integer)
 # Below required once, when creating DB. 
 # db.create_all()
+
 
 @app.route('/')
 def index():
@@ -52,18 +70,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/create-listing', methods=["GET", "POST"])
+@app.route('/create_listing', methods=["GET", "POST"])
 @login_required
 def create_listing():
     form = BookForm()
     if form.validate_on_submit():
-        new_book = Book(title = request.form.get("Title"),
-                        isbn = request.form.get("ISBN"),
-                        author=request.form.get("Author"),
-                        owner= current_user.username)
-        
-        db.session.add(new_book)
-        db.session.commit()
+        new_book = Book(isbn = int(form.isbn.data),
+                        owner= current_user.user_id)
+        session = Session()
+        session.add(new_book)
+        session.commit()
 
         return redirect(url_for("data"))
     # Very important, make sure to initialize the field of the OWNER's
@@ -86,14 +102,14 @@ def data():
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-
     # TODO: If user is already Loggedin
     if current_user.is_authenticated == True:
         return redirect(url_for("index"))
 
     form = RegisterForm()
     if form.validate_on_submit():
-        if User.query.filter_by(email=form.email.data).first():
+        session = Session()
+        if session.query(User).filter(User.email==form.email.data).first():
             #User already exists
             msg = "User with this email already exist, log in instead!"
             return redirect(url_for('login'))
@@ -108,8 +124,9 @@ def register():
             username=form.username.data,
             password=hashed_salted_password,
         )
-        db.session.add(new_user)
-        db.session.commit()
+        
+        session.add(new_user)
+        session.commit()
         login_user(new_user)
         return redirect(url_for("index"))
 
@@ -128,9 +145,9 @@ def login():
     if form.validate_on_submit():
         email = form.email.data
         password = form.password.data
-        
+        session = Session()
         # User from database
-        new_user = User.query.filter_by(email=email).first()
+        new_user = session.query(User).filter(User.email==email).first()
 
         # Wrong email
         if (not new_user) or (not check_password_hash(new_user.password, password)): 
@@ -151,5 +168,17 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+@app.route('/book_list', methods=["GET", "POST"])
+def listings():
+    session = Session()
+    return render_template('book_list.html', query=session.query(Book))
+
+@app.route('/<string:name>/listing', methods=["GET", "POST"])
+def listing(name):
+    session = Session()
+    return render_template('book_info.html', book=session.query(Book).filter(Book.book_id==name).first())
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
